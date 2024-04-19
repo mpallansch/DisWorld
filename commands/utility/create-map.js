9f638911181a7ce73f533ede0412b539ea435266
mpallansch <matthew.pallansch@gmail.com>
2024-04-19 14:10:31 -0400

    Changes Permissions for Create and Remove

--- commands/utility/create-map.js
+++ commands/utility/create-map.js
@@ -1,7 +1,8 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 
 export default {
 	data: new SlashCommandBuilder()
 		.setName('create-map')
 		.setDescription('Creates a DisWorld map in this channel to show user submitted locations')
+	  .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
 };
--- commands/utility/remove-map.js
+++ commands/utility/remove-map.js
@@ -1,7 +1,8 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 
 export default {
 	data: new SlashCommandBuilder()
 		.setName('remove-map')
 		.setDescription('Removes DisWorld map in this channel')
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
 };
